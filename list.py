521a51c08417523b9b1761ec82ce353923449177
unknown <anjila078@gmail.com>
2026-05-26 15:40:55 +0545

    first commit

---
+++ list.py
@@ -0,0 +1,26 @@
+mylist =list(("10" , "20" , "30" , "40" , "50"))
+print(mylist[0])
+print(mylist[-2])
+
+print(mylist[1:4])
+print(mylist[::-3])
+
+tuple1 = ("A" , "B" , "C" , "D" , "E")
+tuple2 = (1, 2, 3, 4, 5)
+tuple3 = ("true", "true", "false")
+
+
+
+print(tuple1)
+print(tuple2)
+print(tuple3)
+
+
+
+pokhara = {
+    "district" : "kaski",
+    "country" : "Nepal",
+    "university" : "pokhara university"
+}
+
+print(pokhara["university"])
