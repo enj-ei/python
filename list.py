7b265fd98527f7bb7619084af03e955d919f6255
unknown <anjila078@gmail.com>
2026-05-27 08:07:36 +0545

    initial commit

--- list.py
+++ list.py
@@ -1,4 +1,4 @@
-mylist =list(("10" , "20" , "30" , "40" , "50"))
+mylist =list(("Ram" , "Sam" , "Sita" , "Amy" , "Prit"))
 print(mylist[0])
 print(mylist[-2])
 
@@ -23,4 +23,51 @@ pokhara = {
     "university" : "pokhara university"
 }
 
+
+pokhara["university"] = "kathmandu university"
 print(pokhara["university"])
+
+
+#to print list into tuples
+my_list = [1, 2, 3, 4, 5]
+my_tuple = tuple(my_list)
+print(my_tuple)
+
+
+my_string = "hello "
+my_tuple = tuple(my_string)
+print(my_tuple)
+
+
+#to convery tuple into list
+my_tuple = ["h" , "e" , "l" , "l" , "o"]
+my_list = list(my_tuple)
+print(my_list)
+
+my_tuple = tuple(my_string)
+print(my_tuple)
+
+
+my_tuple = ("h" , "e" , "l" , "l" , "o")
+
+my_string = ''.join(my_tuple)
+print(my_string)
+
+
+#dict to tuple
+my_dict = {"name": "Ram" , "age":20, "class":10}
+my_tuple= tuple(my_dict.items)
+print(my_tuple)
+
+#tuple to dict
+my_tuple = (('name','Ram'),('age',20),('class',10))
+my_dict= dict(my_tuple)
+print(my_dict)
+
+#dict to list
+my_dict = {"name": "Ram" , "age":20, "class":10}
+my_list = list(my_dict.items())
+print(my_list)
+
+
+
