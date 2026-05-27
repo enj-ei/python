7e044d9995539315631a41f9d4338e5fbc8ed00a
unknown <anjila078@gmail.com>
2026-05-27 08:14:03 +0545

    first

---
+++ set.py
@@ -0,0 +1,13 @@
+S={1,2,3,4}
+fruits = {"apple", "banana", "cherry"}
+print(fruits)
+
+print(type(fruits))
+
+numbers ={1,2,3,4,4,5}
+
+print(numbers)
+
+colors = {"red","blue","green"}
+print(colors)
+
